49fbdee1fd69f961f37eb0b065135897d93b8e4e
cgaroui <162324725+cgaroui@users.noreply.github.com>
2024-03-18 14:40:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+spl_autoload_register(function($class_name) {
+    require 'classes/'.$class_name . '.php';
+});
+
+$hotel1 = new Hotel("Hilton****Strasbourg","10 route de la gare 67000 STRASBOURG");
+
+$chambre1 = new Chambre(1,2,120,true,false,$hotel1);
+$chambre2 = new Chambre(2,1,110,false,true,$hotel1);
+
+
+echo $hotel1->countChambres();
+
+echo $hotel1->countChambresReserves();
